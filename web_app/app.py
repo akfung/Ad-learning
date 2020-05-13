84669917852410db4694dae2fb7b5b320df3ddbf
Amos Fung <amosfung@amoss-mbp.home>
2020-05-13 16:47:54 -0700

    added postgres table insertion on intialization to flask app

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from flask import (
     Flask,
     render_template,
@@ -7,11 +8,12 @@ from flask import (
     redirect)
 
 from flask_sqlalchemy import SQLAlchemy
+import ads_api #module for calling the facebook ads library API
 
 #flask setup
 app = Flask(__name__)
 
-#set different database depending on dev or local database
+#set different database depending on dev or heroku database
 ENV = 'dev'
 
 if ENV == 'dev':
@@ -24,6 +26,7 @@ else:
 #database setup
 db = SQLAlchemy(app)
 
+#class object for political_ads table
 class political_ads(db.Model):
     __tablename__ = 'political_ads'
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +37,7 @@ class political_ads(db.Model):
     Impressions = db.Column(db.String(200))
     Currency = db.Column(db.String(200))
     Ad_Spending = db.Column(db.String(200))
-
+    #initialization functions
     def __init__(self, id, Ad_ID, Ad_URL, Ad_Text, Hosted_Page, Impressions, Currency, Ad_Spending):
         self.id = id
         self.Ad_ID = Ad_ID
@@ -49,9 +52,38 @@ class political_ads(db.Model):
 # create route that renders index.html template
 @app.route("/")
 def home():
+    #on load clear previous tables and populate the database with some ad results
+    db.drop_all()
+    # db.create_all()
+    csv_data = pd.read_csv('../Data/20200511.csv') #read the csv to csv_data
+    csv_data.to_sql('political_ads', db.engine) #write the pandas df to postgres df
+
     return render_template("index.html")
 
-@app.route("/send", methods=["GET", "POST"])
+
+
+#route that goes to visualization page
+@app.route("/visualization", methods=["GET"])
+def visualization():
+    _data = [{
+        "type": "scattergeo",
+        "locationmode": "USA-states",
+        "lat": lat,
+        "lon": lon,
+        "text": hover_text,
+        "hoverinfo": "text",
+        "marker": {
+            "size": 50,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
+    }]
+    return render_template("ad_visualizations.html")
+
+#route that adds to db
+@app.route("/querryDB", methods=["GET", "POST"])
 def send():
     if request.method == "POST":
         name = request.form["petName"]
