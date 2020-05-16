1a0c89b4c7ab5b633388236199e9c1fd9942e362
Amos Fung <amosfung@amoss-mbp.home>
2020-05-16 04:18:34 -0700

    heroku

--- web_app/app.py
+++ web_app/app.py
@@ -65,7 +65,7 @@ class political_ads(db.Model):
 def home():
     #on load clear previous tables and populate the database with some ad results
     db.drop_all()
-    csv_data = pd.read_csv("../Data/20200514") #read the csv to csv_data
+    csv_data = pd.read_csv("https://ad-learning.s3-us-west-1.amazonaws.com/20200514.csv") #read the csv to csv_data
     csv_data.to_sql('political_ads', db.engine) #write the pandas df to postgres
 
     return render_template("index.html")
